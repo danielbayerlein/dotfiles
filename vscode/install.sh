710dbbba7e4e6db3c835bbbca4cd207f2800ef92
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2020-11-30 15:51:00 +0100

    Fixed linking issue

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,8 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 target=$HOME/Library/Application\ Support/Code/User/settings.json
-if [ ! -L $target ]; then
+if [ ! -L "$target" ]; then
   linkable=$DIR/settings.json
   echo "🔗 Linking $linkable → $target"
-  ln -s $linkable $target
+  ln -s $linkable "$target"
 fi
