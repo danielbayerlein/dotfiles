8d0684597b633f02a94c51a8182f7c9ea90206e2
Daniel Bayerlein <danielbayerlein@users.noreply.github.com>
2020-12-18 09:23:15 +0100

    Remove step for install Vim plugins

--- vim/install.sh
+++ vim/install.sh
@@ -5,8 +5,3 @@ if [ ! -f ~/.vim/autoload/plug.vim ]; then
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
-
-if [ ! vim +PlugStatus +qall ]; then
-  echo '💾 Installing Vim plugins'
-  vim +PlugInstall +qall
-fi
